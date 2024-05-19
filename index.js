4a8a42721c3b6832747daa8271f753033eea3290
clayto-nr <claytonrennan01@gmail.com>
2024-05-19 20:57:35 -0300

    add cors

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
+app.use(cors());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
